b3e0e10e4e7f9b8f7da7dddc02c9ef1a1e573526
Kristina Dachtler <kristina.dachtler@t-online.de>
2020-05-24 15:09:33 +0200

    Login Controller erstellt

---
+++ Radverleih/RadverleihWebApp/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RadverleihLib;
+
+namespace RadverleihWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private RadverleihModelContainer db = new RadverleihModelContainer();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View(db.Kundes.ToList());
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kunde kunde = db.Kundes.Find(id);
+            if (kunde == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kunde);
+        }
+    }
+}
